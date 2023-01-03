11a6d99730756ce03eb7924f9c92d1efeca651a1
Poweranimal-web <nikita.guseff@gmail.com>
2023-01-03 23:11:58 +0200

    add admin panel and roles

---
+++ static/js/news/news.js
@@ -0,0 +1,14 @@
+$(document).ready(function(e) {   
+    
+    // $(".card-img-top").css({
+    //     "transition": "transform .2s",
+    //     "overflow": "hidden",
+    // });
+//     $(".card-img-top").hover(function () {
+//         $(this).toggle('animate');
+        
+//     }, function () {
+//         $(this).toggle('usual');
+//     }
+// );
+});
---
+++ static/js/reg/role.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//     $(".btn").hover(function () {
+//         $(this).animate({
+//             "height":"+=50",
+//         },300,function(){});
+//         $(this).siblings(".img").animate({
+//             "height":"+=50",
+//         },300,function(){});    
+//         }, function () {
+//             $(this).animate({
+//                 "height":"-=50",
+//             },300,function(){});
+//             $(this).siblings(".img").animate({
+//                 "height":"-=50",
+//             },300,function(){}); 
+//         }
+//     );
+// });
